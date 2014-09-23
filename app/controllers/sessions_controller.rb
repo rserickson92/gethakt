2d42871350d9b3df0cdc47b1d315e822863ac53c
rserickson92 <rserickson92@gmail.com>
2014-09-22 23:31:07 -0400

    implement authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	params = session_params
+  	user = User.authenticate(params[:name], params[:password])
+  	if user
+  	  session[:user_id] = user.id 
+  		redirect_to posts_path
+  	else
+  		redirect_to root_path
+  	end
   end
 
   def destroy
+  	session[:user_id] = nil
+  	redirect_to root_path
+  end
+
+  private
+
+  def session_params
+  	params.require(:user).permit(:name, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :password, presence: true, length: { in: 6..20 }
+
+  def self.authenticate(name, password)
+  	user = find_by_name(name)
+  	(user && user.password == password) ? user : nil
+  end
 end
